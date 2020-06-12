0e740db5f4086a4b7bdcecbeea4360d2a4c53a7f
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-06-12 18:13:56 +0530

    Added Example 1 Base Project
    - Added example 1 - Using SWRevealViewController
    - Added Base project and structure

---
+++ Example1_SWRevealViewController/Example1_SWRevealViewController/HelperClasses/SWRevealViewController.h
@@ -0,0 +1,17 @@
+//
+//  SWRevealViewController.h
+//  Example1_SWRevealViewController
+//
+//  Created by Mallikarjun on 12/06/20.
+//  Copyright © 2020 Mallikarjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SWRevealViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example1_SWRevealViewController/Example1_SWRevealViewController/HelperClasses/SWRevealViewController.m
@@ -0,0 +1,13 @@
+//
+//  SWRevealViewController.m
+//  Example1_SWRevealViewController
+//
+//  Created by Mallikarjun on 12/06/20.
+//  Copyright © 2020 Mallikarjun. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
+
+@implementation SWRevealViewController
+
+@end
