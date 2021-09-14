036970d1f62b9109d6aace901a5722ee4c6eb4ea
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-09-14 12:18:44 +0530

    update for custom sidemenu

--- Custom_SideMenu/Custom_SideMenu/SideMenu/SideMenuViewController.swift
+++ Custom_SideMenu/Custom_SideMenu/SideMenu/SideMenuViewController.swift
@@ -7,23 +7,51 @@
 
 import UIKit
 
+protocol SideMenuViewControllerDelegate {
+    func selectedCell(_ row: Int)
+}
+
 class SideMenuViewController: UIViewController {
 
+    @IBOutlet weak var mainTableView: UITableView!
+   
+    var defaultHighlightedCell: Int = 0
+    
+    var delegate: SideMenuViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Update TableView with the data
+        self.mainTableView.reloadData()
+    }
+
+}
+
+extension SideMenuViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+         return 4
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SideMenuCell", for: indexPath) as! SideMenuCell
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        self.delegate?.selectedCell(indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 55.0
+    }
+    
 }
